b6cae7ed1904807857392fa5c197525a10133ec1
Luan <luan@Karols-MacBook-Air.local>
2024-04-20 22:09:05 -0400

    add a custom hook

---
+++ hooks/useChat.tsx
@@ -0,0 +1,95 @@
+import { useState, useCallback } from "react";
+import EventSource from "react-native-sse";
+
+export const useChat = () => {
+
+    const [conversations, setConversations] = useState([]);
+    const [aiMessage, setAiMessage] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+
+    const OpenAIToken = process.env.EXPO_PUBLIC_OPENAI_KEY;
+    const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+    const handleNewMessage = useCallback(async () => {
+
+        if (userMessage === '') {
+            return;
+        }
+
+        const es = new EventSource(
+            API_URL,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${OpenAIToken}`,
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    model: "gpt-3.5-turbo-0125",
+                    messages: [
+                        {
+                            role: "system",
+                            content: "You are a helpful assistant.",
+                        },
+                        {
+                            role: "user",
+                            content: userMessage,
+                        },
+                    ],
+                    max_tokens: 600,
+                    n: 1,
+                    temperature: 0.7,
+                    stream: true,
+                }),
+                pollingInterval: 0,
+            }
+        );
+
+        es.addEventListener("open", () => {
+            setConversations((prev) => {
+                return [
+                    ...prev,
+                    {
+                        message: userMessage,
+                        isHuman: true,
+                    },
+                ];
+            });
+
+            setUserMessage("");
+        });
+
+        es.addEventListener("message", (event) => {
+            if (event.data !== "[DONE]") {
+                const data = JSON.parse(event.data);
+
+                if (data.choices[0].delta.content !== undefined) {
+                    setAiMessage((text) => text + data.choices[0].delta.content);
+                }
+            }
+        });
+
+        setConversations((prev) => {
+            return [
+                ...prev,
+                {
+                    message: aiMessage,
+                    isHuman: false,
+                },
+            ];
+        });
+
+        setAiMessage("");
+
+        return () => {
+            es.removeAllEventListeners();
+            es.close();
+        };
+
+    }, [userMessage]);
+
+
+
+
+    return { handleNewMessage, conversations, aiMessage, userMessage, setUserMessage }
+}
--- screens/Chat/index.tsx
+++ screens/Chat/index.tsx
@@ -1,93 +1,10 @@
-import { useState, useCallback } from "react";
 import { styles } from './style';
 import { Text, View, TextInput, TouchableOpacity, Image, ScrollView, SafeAreaView } from 'react-native';
-import EventSource from "react-native-sse";
+import { useChat } from 'hooks/useChat';
 
 const Chat = () => {
-  const [conversations, setConversations] = useState([]);
-  const [aiMessage, setAiMessage] = useState('');
-  const [userMessage, setUserMessage] = useState('');
 
-  // SSE
-  const OpenAIToken = process.env.EXPO_PUBLIC_OPENAI_KEY;
-
-  const handleNewMessage = useCallback(async () => {
-
-    if (userMessage === '') {
-      return;
-    }
-
-    const es = new EventSource(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${OpenAIToken}`,
-        },
-        method: "POST",
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo-0125",
-          messages: [
-            {
-              role: "system",
-              content: "You are a helpful assistant.",
-            },
-            {
-              role: "user",
-              content: userMessage,
-            },
-          ],
-          max_tokens: 600,
-          n: 1,
-          temperature: 0.7,
-          stream: true,
-        }),
-        pollingInterval: 0,
-      }
-    );
-
-    es.addEventListener("open", () => {
-      setConversations((prev) => {
-        return [
-          ...prev,
-          {
-            message: userMessage,
-            isHuman: true,
-          },
-        ];
-      });
-
-      setUserMessage("");
-    });
-
-    es.addEventListener("message", (event) => {
-      if (event.data !== "[DONE]") {
-        const data = JSON.parse(event.data);
-
-        if (data.choices[0].delta.content !== undefined) {
-          setAiMessage((text) => text + data.choices[0].delta.content);
-        }
-      }
-    });
-
-    setConversations((prev) => {
-      return [
-        ...prev,
-        {
-          message: aiMessage,
-          isHuman: false,
-        },
-      ];
-    });
-
-    setAiMessage("");
-
-    return () => {
-      es.removeAllEventListeners();
-      es.close();
-    };
-
-  }, [userMessage]);
+  const { handleNewMessage, conversations, aiMessage, userMessage, setUserMessage } = useChat();
 
   return (
     <SafeAreaView style={styles.safeArea}>
